849e9e8ab65ce8d7503c55f746a537fbf61366b7
chocogruh <mthiesso@student.42lausanne.ch>
2023-01-12 15:05:33 +0100

    mlx_change

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ggentil <ggentil@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mthiesso <mthiesso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 16:47:23 by mthiesso          #+#    #+#             */
-/*   Updated: 2023/01/10 15:42:54 by ggentil          ###   ########.fr       */
+/*   Updated: 2023/01/12 15:04:49 by mthiesso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@ int	init_game(t_data *dt)
 	dt->img = ft_calloc(1, sizeof(t_imgptr));
 	dt->mlx = mlx_init();
 	dt->window = mlx_new_window(dt->mlx, WIN_X, WIN_Y, "Les gentils seaux");
-	dt->img->img = mlx_new_image(dt->mlx, WIN_X, WIN_Y);
-	dt->img->path = mlx_get_data_addr(dt->img->img, &dt->img->bytes,
-			&dt->img->line, &dt->img->end);
 	dt->tablen = 0;	
 	return (0);
 }
--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: mthiesso <mthiesso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 16:42:27 by mthiesso          #+#    #+#             */
-/*   Updated: 2023/01/05 17:34:27 by mthiesso         ###   ########.fr       */
+/*   Updated: 2023/01/12 15:04:30 by mthiesso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int	screen_display(t_data *dt)
 {
+	dt->img->img = mlx_new_image(dt->mlx, WIN_X, WIN_Y);
+	dt->img->path = mlx_get_data_addr(dt->img->img, &dt->img->bytes,
+			&dt->img->line, &dt->img->end);
 	minimap_display(dt);
 	mlx_put_image_to_window(dt->mlx, dt->window, dt->img->img, 0, 0);
+	mlx_destroy_image(dt->mlx, dt->img->img);
 	return (0);
 }
