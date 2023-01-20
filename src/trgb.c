fee04b7060a7a6807036a43e45886ad70c62149e
Gabriela Gentil <ggentil@student.42lausanne.ch>
2023-01-20 16:00:14 +0100

    add trgb

---
+++ src/trgb.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggentil <ggentil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 14:51:48 by ggentil           #+#    #+#             */
+/*   Updated: 2023/01/20 11:14:03 by ggentil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int	get_transparency(int trgb)
+{
+	return ((trgb >> 24) & 0xFF);
+}
+
+int	get_red(int trgb)
+{
+	return ((trgb >> 16) & 0xFF);
+}
+
+int	get_green(int trgb)
+{
+	return ((trgb >> 8) & 0xFF);
+}
+
+int	get_blue(int trgb)
+{
+	return (trgb & 0xFF);
+}
